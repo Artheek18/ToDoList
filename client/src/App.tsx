c3b933d9370cd2e57c008da50337abd51ee4dc2e
Artheek18 <artheek2321.Shan@gmail.com>
2024-11-19 02:55:47 -0500

    started the frontend using Vite+React

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@chakra-ui/react"
+import { HStack } from "@chakra-ui/react"
+
+function App() {
+
+  return (
+    <>
+    <HStack h="100vh">
+      <Button>Click me</Button>
+    </HStack>
+    </>
+  )
+}
+
+export default App
